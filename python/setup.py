7474670fbe12b657bf93eb5e711576e00564a383
Joachim <joachim.hahn@mpi-hd.mpg.de>
2015-11-17 20:11:56 +0100

    fixed python/setup.py

--- python/setup.py
+++ python/setup.py
@@ -3,8 +3,7 @@ import string
 import subprocess
 GSL_LIBS = string.split(subprocess.check_output(['gsl-config','--libs']))
 GSL_CFLAGS = string.split(subprocess.check_output(['gsl-config','--cflags']))
-#GSL_LIBS = pkgconfig.libs('gsl')
-print "->",GSL_LIBS
+
 if(GSL_CFLAGS):
   COMPILEARGS=GSL_CFLAGS
   COMPILEARGS.append('-std=c++11')
@@ -14,8 +13,6 @@ else:
 extension_mod = Extension("_gappa",
                           ["_gappa.cc", "../src/Radiation.C","../src/Particles.C","../src/Utils.C","../src/Astro.C"],
                           extra_compile_args=COMPILEARGS,
-                          #libraries=['gsl','gslcblas','m'],
-                          #libraries=GSL_LIBS,
                           extra_link_args=GSL_LIBS,
                           include_dirs=['../include'],
 )
