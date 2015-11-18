43ffd88c82cff47e9b740b6590658d03192ea199
Joachim <joachim.hahn@mpi-hd.mpg.de>
2015-11-18 01:44:38 +0100

    fixed the travis.yml file to make gappa compile on mac with gcc (had to remove the shorten-64-to-32 warning flag).

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup, Extension
 import string
+import sys
 import subprocess
 GSL_LIBS = subprocess.check_output(['gsl-config','--libs']).split()
 GSL_CFLAGS = subprocess.check_output(['gsl-config','--cflags']).split()
@@ -9,6 +10,9 @@ if(GSL_CFLAGS):
   COMPILEARGS.append('-std=c++11')
 else:
   COMPILEARGS=['-std=c++11']
+
+if sys.platform == 'darwin':
+  COMPILEARGS.append('-Wno-error=shorten-64-to-32')
 
 extension_mod = Extension("_gappa",
                           ["_gappa.cc", "../src/Radiation.C","../src/Particles.C","../src/Utils.C","../src/Astro.C"],
