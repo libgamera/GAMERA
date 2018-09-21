24e81bb98dea4ab761085286c3b55a18d056f2b1
JoachimHahn <joachim.hahn@mpi-hd.mpg.de>
2018-09-21 13:33:48 +0200

    fixed radiation_autotest.py

--- autotest/radiation_autotest.py
+++ autotest/radiation_autotest.py
@@ -4,7 +4,6 @@ import sys
 sys.path.append('lib')
 import gappa as gp
 import numpy as np
-import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     e_ref = gp.TeV_to_erg
@@ -52,32 +51,4 @@ if __name__ == "__main__":
     print "ic_sed:"
     print ic_sed
     print "-------------"
-
-
-    f,ax = plt.subplots(figsize=(5,5))
-    ax.set_prop_cycle('color',plt.get_cmap('plasma')(np.linspace(0., .8, 2))) #
-    plt.loglog(energy_in_erg_pl,1e49*power_law,label="protons")
-    plt.loglog(energy_in_erg_pl,1e47*power_law,label="electrons")
-    plt.xlabel("Energy (erg)")
-    plt.ylabel("dN/dE (1/erg)")
-    plt.ylim(ymin=1e31,ymax=1e55)
-    plt.legend()
-    plt.title("particle spectra")
-    plt.grid()
-    f.savefig("autotest/particle_spectra.png",bbox_inches='tight')
-
-    f,ax = plt.subplots(figsize=(5,5))
-    ax.set_prop_cycle('color',plt.get_cmap('plasma')(np.linspace(0., .8, 5)))  #
-    plt.loglog(total_sed[:,0],total_sed[:,1],lw=4,alpha=0.2,label="sum")
-    plt.loglog(pp_sed[:,0],pp_sed[:,1],label="p-p")
-    plt.loglog(synch_sed[:,0],synch_sed[:,1],label="synchrotron")
-    plt.loglog(brems_sed[:,0],brems_sed[:,1],label="bremsstrahlung")
-    plt.loglog(ic_sed[:,0],ic_sed[:,1],label="inverse-compton")
-    plt.xlabel("Energy (TeV)")
-    plt.ylabel("E^2dN/dE (erg/cm^2/s)")
-    plt.ylim(ymin=1e-18,ymax=1e-8)
-    plt.legend()
-    plt.grid()
-    plt.title("Radiation SEDs")
-    f.savefig("autotest/radiation_SEDs.png",bbox_inches='tight')
 
